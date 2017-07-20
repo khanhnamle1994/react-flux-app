7c4ba7931ef65974f8e6dc5d9d98aa58136554f2
James Le <le_j6@denison.edu>
2017-07-19 18:24:55 -0700

    App Dispatcher

---
+++ dispatcher/AppDispatcher.js
@@ -0,0 +1,37 @@
+// AppDispatcher.js
+import {Dispatcher} from 'flux';
+let AppDispatcher = new Dispatcher();
+
+import ListStore from '../stores/ListStore';
+
+// Register callback with AppDispatcher
+AppDispatcher.register((payload) => {
+
+  let action = payload.action;
+  let new_item = payload.new_item;
+  let id = payload.id;
+
+  switch(action) {
+
+    // Respond to add-item action
+    case 'add-item':
+      ListStore.addItem(new_item);
+      break;
+
+    // Respond to remove-item action
+    case 'remove-item':
+      ListStore.removeItem(id);
+      break;
+
+    default:
+      return true;
+  }
+
+  // If action was responded to, emit change event
+  ListStore.emitChange();
+
+  return true;
+
+});
+
+export default AppDispatcher;
