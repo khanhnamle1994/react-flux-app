8b0b3eb84557d5321fca5daa5b001217e5404401
James Le <le_j6@denison.edu>
2017-07-19 18:25:55 -0700

    Change List Store

--- stores/ListStore.js
+++ stores/ListStore.js
@@ -16,9 +16,43 @@ let ListStore = _.extend({}, EventEmitter.prototype, {
     }
   ],
 
+  // Get all items
   getItems: function(){
     return this.items;
   },
+
+  // Add item
+  addItem: function(new_item){
+    this.items.push(new_item);
+  },
+
+  // Remove item
+  removeItem: function(item_id){
+
+    let items = this.items;
+
+    _.remove(items,(item) => {
+      return item_id == item.id;
+    });
+
+    this.items = items;
+
+  },
+
+  // Emit Change event
+  emitChange: function(){
+    this.emit('change');
+  },
+
+  // Add change listener
+  addChangeListener: function(callback){
+    this.on('change', callback);
+  },
+
+  // Remove change listener
+  removeChangeListener: function(callback){
+    this.removeListener('change', callback);
+  }
 
 });
 
