b77b2c80a84c6f526da8758234b178c9fb536c1a
James Le <le_j6@denison.edu>
2017-07-19 18:27:14 -0700

    Listen to New Events Triggered by ListStore

--- components/AppRoot.jsx
+++ components/AppRoot.jsx
@@ -1,6 +1,10 @@
 // AppRoot.jsx
 import React from 'react';
 import ListStore from '../stores/ListStore';
+import AppDispatcher from '../dispatcher/AppDispatcher';
+
+// Sub components
+import NewItemForm from './NewItemForm';
 
 // Method to retrieve state from Stores
 let getListState = () => {
@@ -10,25 +14,55 @@ let getListState = () => {
 }
 
 class AppRoot extends React.Component {
+
+  // Method to setState based upon Store changes
+  _onChange() {
+    this.setState(getListState());
+  }
+
   constructor() {
     super();
     this.state = getListState();
   }
+
+  // Add change listeners to stores
+  componentDidMount() {
+    ListStore.addChangeListener(this._onChange.bind(this));
+  }
+
+  // Remove change listeners from stores
+  componentWillUnmount() {
+    ListStore.removeChangeListener(this._onChange.bind(this));
+  }
+
+  removeItem(e){
+
+    let id = e.target.dataset.id;
+
+    AppDispatcher.dispatch({
+      action: 'remove-item',
+      id: id
+    });
+
+  }
+
   render(){
+
+    let _this = this;
     let items = ListStore.getItems();
     let itemHtml = items.map(( listItem ) => {
       return <li key={ listItem.id }>
-          { listItem.name }
+          { listItem.name } <button onClick={ _this.removeItem } data-id={ listItem.id }>×</button>
         </li>;
-
     });
-    return (
-      <div>
+
+    return <div>
         <ul>
-          { itemHtml }
+            { itemHtml }
         </ul>
-      </div>
-    );
+        <NewItemForm />
+
+    </div>;
   }
 
 }
